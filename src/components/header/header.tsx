de6e6cefed48bb271c14206775d67ddd6a2756ee
zsw722 <1129038240@qq.com>
2024-04-19 19:58:02 +0800

    three

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,13 +12,13 @@ export default function Header() {
         <Link
           target="_blank"
           className="scale-100 active:scale-100"
-          href="https://github.com/sunshineLixun"
+          href="https://github.com/Orioner722"
         >
           <Image
             width={32}
             height={32}
             alt="sunshineLixun"
-            src="https://avatars.githubusercontent.com/u/15700015?v=4"
+            src="https://avatars.githubusercontent.com/u/76165623?u=4e9cd6498ca4ebabb6ee7df94de2469af7d43353&v=4"
             className="relative -top-1 mx-2 inline h-8 w-8 rounded-full"
           />
         </Link>
